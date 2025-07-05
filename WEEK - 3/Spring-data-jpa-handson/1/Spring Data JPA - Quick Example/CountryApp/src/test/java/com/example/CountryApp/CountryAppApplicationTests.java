b1896c9ae6da6639ff2f8ea580ab4de9289e7328
SujaayaJayaraman <sujaayajayaraman22@gmail.com>
2025-07-05 13:59:04 +0530

    Adding week 3 hands-on
    Spring Core_Maven, Spring - data - jpa-handson

---
+++ WEEK - 3/Spring-data-jpa-handson/1/Spring Data JPA - Quick Example/CountryApp/src/test/java/com/example/CountryApp/CountryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CountryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
