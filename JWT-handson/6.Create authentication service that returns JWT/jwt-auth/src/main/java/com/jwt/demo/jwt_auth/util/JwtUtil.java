2f7a529c2f2c5b163f76c1bc2b802267e240335d
SujaayaJayaraman <sujaayajayaraman22@gmail.com>
2025-07-13 16:12:05 +0530

    Adding week-4 handson

---
+++ JWT-handson/6.Create authentication service that returns JWT/jwt-auth/src/main/java/com/jwt/demo/jwt_auth/util/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.jwt.demo.jwt_auth.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    // Use JJWT's secure key generator
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 10)) // 10 minutes
+                .signWith(key)
+                .compact();
+    }
+}
